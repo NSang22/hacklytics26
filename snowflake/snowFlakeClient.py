d3490062d5637c8f5293d627fd6fb7b80a3d8ad6
Gael Garcia <gael.garcia@ufl.edu>
2026-02-21 18:05:52 -0500

    adding rest of processing files

--- snowflake/snowFlakeClient.py
+++ snowflake/snowFlakeClient.py
@@ -21,9 +21,9 @@ class SnowflakeClient:
             "account":   account   or os.environ["SNOWFLAKE_ACCOUNT"],
             "user":      user      or os.environ["SNOWFLAKE_USER"],
             "password":  password  or os.environ["SNOWFLAKE_PASSWORD"],
-            "database":  database  or os.environ.get("SNOWFLAKE_DATABASE", "DATAMART_DB"),
-            "schema":    schema    or os.environ.get("SNOWFLAKE_SCHEMA",   "DATAMART_SCHEMA"),
-            "warehouse": warehouse or os.environ.get("SNOWFLAKE_WAREHOUSE", "DATAMART_WH"),
+            "database":  database  or os.environ.get("SNOWFLAKE_DATABASE", "CRASHOUT_DB"),
+            "schema":    schema    or os.environ.get("SNOWFLAKE_SCHEMA",   "CRASHOUT_SCHEMA"),
+            "warehouse": warehouse or os.environ.get("SNOWFLAKE_WAREHOUSE", "CRASHOUT_WH"),
         }
         resolved_role = role or os.environ.get("SNOWFLAKE_ROLE")
         if resolved_role:
